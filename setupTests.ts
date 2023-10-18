59b1946ca80de73b627f5c349043573eba00a655
ZmlAlex <amazonalexzml@gmail.com>
2023-10-19 00:06:40 +0400

    -merge master

--- setupTests.ts
+++ setupTests.ts
@@ -1,13 +1,5 @@
 import { prisma } from "~/server/db";
 
-// mock emails for tests
-jest.mock("./emails/LoginEmail", () => {
-  return {
-    __esModule: true,
-    default: jest.fn(),
-  };
-});
-
 beforeAll(async () => {
   await prisma.user.deleteMany({
     where: {
