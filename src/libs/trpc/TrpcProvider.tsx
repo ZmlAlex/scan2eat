b609ebee5ebe1cd6ddc26bc6c2d1e7594271a059
ZmlAlex <amazonalexzml@gmail.com>
2024-03-18 12:04:30 +0400

    bug: fix url link for trpc

--- src/libs/trpc/TrpcProvider.tsx
+++ src/libs/trpc/TrpcProvider.tsx
@@ -45,8 +45,8 @@ export const TrpcProvider: React.FC<{ children: React.ReactNode }> = ({
   );
 
   const url = process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc/`
-    : "http://localhost:3000/api/trpc/";
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+    : "http://localhost:3000/api/trpc";
 
   const [trpcClient] = useState(() =>
     clientApi.createClient({
